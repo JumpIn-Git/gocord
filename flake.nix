711eb026ae51c30ec1260a53ed55ad0aac1c371a
JumpIn-Git <yaasirali0@hotmail.com>
2026-07-27 16:37:24 +0200

    update envrc and flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "flake:nixpkgs";
+    syntaqlite= {
+      url = "github:lalitmaganti/syntaqlite";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
+  outputs = { nixpkgs, syntaqlite, ... }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
     devShells.x86_64-linux.default = pkgs.mkShellNoCC {
       packages = [
         pkgs.goose
         pkgs.go
+        syntaqlite.packages.x86_64-linux.default
+        pkgs.sqlite-interactive
       ];
     };
   };
